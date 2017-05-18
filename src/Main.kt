356cd5568a1988ac06b20781c112d4d1200b0ee1
Adrián Cepeda Ávila <adriancepeda1310@gmail.com>
2017-05-19 01:12:45 +0200

    Other function

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,10 @@
 fun main(args: Array<String>){
 	println("Hola mundo")
 	println(sumar(1,2))
+	println(mayorDeEdad(17))
+	println(mayorDeEdad(18))
 }
 
 fun sumar(num1:Int,num2:Int)=num1+num2
+
+fun mayorDeEdad(edad:Int) = if(edad>=18) true else false
